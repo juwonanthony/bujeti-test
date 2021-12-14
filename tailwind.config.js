ec5e88db893e85faf27419ab57a27f7f7b751deb
Silas <silasfaarda@gmail.com>
2021-12-14 17:39:44 +0100

    added color variables to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: {
+          orange: '#D28B28',
+          green: '#BFFF0B'
+        },
+        bg: "#F9F1EC"
+      }
+    },
   },
   plugins: [],
 }
