792768534abd703b841b0e31a098a868bd7b9088
Achille AROUKO <achille.arouko@gmail.com>
2022-03-06 21:59:43 +0100

    update description

--- src/components/meta-tags.tsx
+++ src/components/meta-tags.tsx
@@ -4,29 +4,29 @@ import React from "react";
 const MetaTags = () => {
   return (
     <Head>
-      <title>Bujeti - Budgeting and Expense management tools for Africans by Africans</title>
-      <meta name="title" content="Bujeti - Budgeting and Expense management tools for Africans by Africans" />
+      <title>Bujeti - Budgeting and Expense Management software for Africans by Africans</title>
+      <meta name="title" content="Bujeti - Budgeting and Expense Management software for Africans by Africans" />
       <meta
         name="description"
 
-        content="Budgeting and expense management software built for your african realities. It empowers your people to do more. It combines credit cards, payments, expenses, and cash into one experience powered by premium Visa Cards, dedicated NUBANs, Mobile Wallets and Faster Payments."
+        content="Budgeting and expense management software built for your african realities. It empowers your people to do more. It combines virtual, smart physical credit cards, payments and expenses into one experience powered by Visa Cards, dedicated NUBANs, Mobile Wallets and Faster Payments."
       />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://bujeti.com" />
-      <meta property="og:title" content="Bujeti - Budgeting and Expense management tools for Africans by Africans" />
+      <meta property="og:title" content="Bujeti - Budgeting and Expense Management software for Africans by Africans" />
       <meta
         property="og:description"
-        content="Budgeting and expense management software built for your african realities. It empowers your people to do more. It combines credit cards, payments, expenses, and cash into one experience powered by premium Visa Cards, dedicated NUBANs, Mobile Wallets and Faster Payments."
+        content="Budgeting and expense management software built for your african realities. It empowers your people to do more. It combines virtual and smart physical credit cards, payments and expenses into one experience powered by Visa Cards, dedicated NUBANs, Mobile Wallets and Faster Payments."
       />
       <meta property="og:image" content="https://bujeti.com/images/bujeti_banner.png" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://bujeti.com" />
-      <meta property="twitter:title" content="Bujeti - Budgeting and Expense management tools for Africans by Africans" />
+      <meta property="twitter:title" content="Bujeti - Budgeting and Expense Management software for Africans by Africans" />
       <meta
         property="twitter:description"
-        content="Budgeting and expense management software built for your african realities. It empowers your people to do more. It combines credit cards, payments, expenses, and cash into one experience powered by premium Visa Cards, dedicated NUBANs, Mobile Wallets and Faster Payments."
+        content="Budgeting and expense management software built for your african realities. It empowers your people to do more. It combines virtual and smart physical credit cards, payments and expenses into one experience powered by Visa Cards, dedicated NUBANs, Mobile Wallets and Faster Payments."
       />
       <meta property="twitter:image" content="https://bujeti.com/images/bujeti_banner.png" />
       <link rel="icon" href="/favicon.ico" type="image/png" sizes="16x16" />
