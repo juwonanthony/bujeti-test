4f72e9f85df33087f9a2568dad907c598deb0fdd
Achille AROUKO <achille.arouko@gmail.com>
2022-03-06 21:25:30 +0100

    fix images issues, add meta for SEO

--- src/components/meta-tags.tsx
+++ src/components/meta-tags.tsx
@@ -14,22 +14,21 @@ const MetaTags = () => {
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://bujeti.com" />
-      <meta property="og:title" content="Bujeti" />
+      <meta property="og:title" content="Bujeti - Budgeting and Expense management tools for Africans by Africans" />
       <meta
         property="og:description"
-
         content="Budgeting and expense management software built for your african realities. It empowers your people to do more. It combines credit cards, payments, expenses, and cash into one experience powered by premium Visa Cards, dedicated NUBANs, Mobile Wallets and Faster Payments."
       />
-      <meta property="og:image" content="link-to-banner-here" />
+      <meta property="og:image" content="https://bujeti.com/images/bujeti_banner.png" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://bujeti.com" />
-      <meta property="twitter:title" content="Bujeti" />
+      <meta property="twitter:title" content="Bujeti - Budgeting and Expense management tools for Africans by Africans" />
       <meta
         property="twitter:description"
         content="Budgeting and expense management software built for your african realities. It empowers your people to do more. It combines credit cards, payments, expenses, and cash into one experience powered by premium Visa Cards, dedicated NUBANs, Mobile Wallets and Faster Payments."
       />
-      <meta property="twitter:image" content="link-to-banner-here" />
+      <meta property="twitter:image" content="https://bujeti.com/images/bujeti_banner.png" />
       <link rel="icon" href="/favicon.ico" type="image/png" sizes="16x16" />
     </Head>
   );
