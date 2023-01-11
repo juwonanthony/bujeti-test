ce9434ec20266d5339012820656de025f5fb7c42
David Johnson <davemoses3@gmail.com>
2023-01-11 13:27:55 +0100

    fix svg

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d220abc9120e80a8"; }
+/******/ 		__webpack_require__.h = function() { return "fc7a29cadf4c66e1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
