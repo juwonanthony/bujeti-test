7fdc803c9b674fe90433dc4bde222ab94da77e01
David Johnson <davemoses3@gmail.com>
2023-02-16 12:47:59 +0100

    erge

--- components/partner-logo/index.jsx
+++ components/partner-logo/index.jsx
@@ -4,15 +4,14 @@ import React from 'react'
 const PartnerLogo = ({ column, image = [] }) => {
   return (
     <div
-      className={`mb-10 grid gap-20 lg:gap-3`}
+      className={`stats mb-10 grid gap-36 lg:gap-3`}
       style={{ gridTemplateColumns: `repeat(${column}, minmax(0, 1fr))` }}
     >
       {image.map(({ image: { filename } }, index) => (
-        <div
-          key={index}
-          className="relative aspect-[4/1] w-40 shrink-0 overflow-scroll md:overflow-hidden lg:overflow-hidden"
-        >
-          <Image fill src={filename} alt={`${filename}-${index}`} className="w-ful h-full" />
+        <div className="stat h-fit place-items-center" key={index}>
+          <div className="relative aspect-[4/1] w-40 shrink-0 ">
+            <Image fill src={filename} alt={`${filename}-${index}`} className="w-ful h-full" />
+          </div>
         </div>
       ))}
     </div>
