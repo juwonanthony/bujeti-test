2a16e0f14ab18913f4ea1bf8ba9d8b64d72c5072
David Johnson <davemoses3@gmail.com>
2023-03-24 22:42:47 +0100

    erge

--- containers/home/hero.jsx
+++ containers/home/hero.jsx
@@ -8,7 +8,7 @@ import Playicon from '../../assets/icons/play.svg'
 const Hero = ({ hero }) => {
   return (
     <section className="min-h-full bg-bg_overlay bg-cover bg-no-repeat pt-10 md:pt-0 lg:h-screen 2xl:h-full">
-      <div className="grid items-center md:grid-cols-2 lg:pl-24">
+      <div className="grid items-center gap-2 md:grid-cols-2 lg:pl-24">
         <div className="flex w-full max-w-3xl flex-col items-center px-6 text-center md:items-start md:text-start lg:px-0 xl:-mt-44">
           <h1 className="title-font mb-4 text-3xl font-normal text-accent-gray sm:text-4xl lg:text-7xl">
             {HTMLReactParser(hero?.title)}
@@ -16,7 +16,7 @@ const Hero = ({ hero }) => {
           <p className="mb-8 font-light leading-relaxed text-accent-gray md:text-xl">
             {hero?.subtitle}
           </p>
-          <div className="flex w-fit flex-row gap-3">
+          <div className="flex w-fit flex-row gap-1">
             {/* <div className="flex flex-col justify-center gap-4 md:flex-row md:gap-0 lg:flex-row lg:gap-0"> */}
             <a
               href={hero.primary_btn.url}
