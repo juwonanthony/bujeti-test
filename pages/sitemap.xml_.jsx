944059cf85ca7fdbee03f17d1c7e58699cd82865
David Johnson <davemoses3@gmail.com>
2023-04-03 23:42:12 +0100

    remove product carosel

--- pages/sitemap.xml_.jsx
+++ pages/sitemap.xml_.jsx
@@ -1,13 +1,15 @@
 // import { sanityClient } from 'lib/sanity-server'
 // import { postSlugsQuery } from 'lib/queries'
 
+import { pages } from 'utils'
+
 const createSitemap = (slugs) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${slugs
           .map((slug) => {
             return `
                 <url>
-                    <loc>${`https://leerob.io/${slug}`}</loc>
+                    <loc>${`https://bujeti.com/${slug}`}</loc>
                 </url>
             `
           })
@@ -16,9 +18,10 @@ const createSitemap = (slugs) => `<?xml version="1.0" encoding="UTF-8"?>
 `
 export async function getServerSideProps({ res }) {
   // const allPosts = await sanityClient.fetch(postSlugsQuery)
+
   const allPages = [
     // ...allPosts.map((slug) => `blog/${slug}`),
-    ...['', 'customers', 'industries', 'products'],
+    ...['', 'about', 'careers', 'ontact-us', 'terms-of-use', 'privacy', ...pages],
   ]
 
   res.setHeader('Content-Type', 'text/xml')
